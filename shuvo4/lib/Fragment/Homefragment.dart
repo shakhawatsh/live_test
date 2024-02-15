414d9549713e6397b6f5416b6f947bb1c51a8905
hp <shakhawatshuvo1@gmail.com>
2024-02-15 23:29:43 +0600

    name updated

---
+++ shuvo4/lib/Fragment/Homefragment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Homefragment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home Fragment Tab!"),
+      ),
+    );
+  }
+
+}
---
+++ shuvo4/lib/Fragment/Videosfragment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Videosfragment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Videos Fragment Tab!"),
+      ),
+    );
+  }
+
+}
