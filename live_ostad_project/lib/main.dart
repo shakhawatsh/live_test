921b6c3f7b2eda1be729a958cc9fb78f1321d5e1
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-04-07 03:26:00 +0600

    8 class 2

--- live_ostad_project/lib/main.dart
+++ live_ostad_project/lib/main.dart
@@ -1,55 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:live_ostad_project/Profile.dart';
-import 'package:live_ostad_project/Settings.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        textButtonTheme: TextButtonThemeData(style: ButtonStyles.textbuttonstyle,),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyles.elevatedstyle,)
+      ),
+    );
   }
-}
 
-class HomePage extends StatelessWidget {
+}
+class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
         backgroundColor: Colors.green,
         leading: Icon(Icons.home),
+        title: Text('Home'),
       ),
+
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Profile(
-                      Username: 'shuvo',
-                    );
-                  })).then((value){
-                    print(value);
-                  });
-                },
-                child: Text('Go to Profile')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Settings(
-                      Name: 'shuvo',
-                    );
-                  }));
-                },
-                child: Text('Settings')),
-          ], // children
+            ElevatedButton(onPressed: (){}, child: Text('Button 1')),
+            ElevatedButton(onPressed: (){}, child: Text('Button 2')),
+            ElevatedButton(onPressed: (){}, child: Text('Button 3')),
+
+            TextButton(onPressed: (){}, child:Text('Text button 1')),
+            TextButton(onPressed: (){}, child:Text('Text button 2')),
+            TextButton(onPressed: (){}, child:Text('Text button 3')),
+          ],
         ),
       ),
     );
   }
+
+}
+
+class ButtonStyles{
+  static ButtonStyle elevatedstyle= ElevatedButton.styleFrom(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+    ),
+
+    backgroundColor: Colors.green,
+    foregroundColor: Colors.white,
+    textStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)
+  );
+
+  static ButtonStyle textbuttonstyle= TextButton.styleFrom(
+    backgroundColor: Colors.green,
+    foregroundColor: Colors.black,
+
+  );
 }
