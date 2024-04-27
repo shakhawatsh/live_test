cd49f3e86424cbc16310721bcfe09b135d720f0d
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-04-27 13:20:33 +0600

    new

--- assignment2/lib/main.dart
+++ assignment2/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -12,27 +12,33 @@ class MyApp extends StatelessWidget{
       home: Home(),
     );
   }
-
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back_ios),iconSize: 20),
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back_ios),
+            iconSize: 20,style: IconButton.styleFrom(foregroundColor: Colors.white,backgroundColor: Colors.green, shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(8))),),
         backgroundColor: const Color.fromRGBO(44, 171, 0, 1),
-        title: Text('Photo Gallery',style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+        title: Text(
+          'Photo Gallery',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert_sharp), style: ButtonStyle()),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert_sharp),
+              style: IconButton.styleFrom(foregroundColor: Colors.white,)),
         ],
         elevation: 70,
       ),
     );
   }
-
-
 }
