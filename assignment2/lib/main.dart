aed271d12f14e1acc9d5971ac1ddc0894fd3f792
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-04-29 01:34:06 +0600

    april 28

--- assignment2/lib/main.dart
+++ assignment2/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:assignment2/mood.dart';
+import 'package:assignment2/home_land.dart';
+import 'package:assignment2/home_port.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
@@ -25,43 +26,25 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+   const Home({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
+      body: OrientationBuilder(builder: (context, orientation) {
+        if(orientation == Orientation.landscape){
+          return  HomeLand();
+        }else{
+          return  HomePort();
+        }
+      },),
 
-          },
-          icon: const Icon(Icons.arrow_back_ios),
-          iconSize: 20,
-          style: IconButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.green,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8))),
-        ),
-        backgroundColor: const Color.fromRGBO(44, 171, 0, 1),
-        title: const Text(
-          'Photo Gallery',
-          style: TextStyle(fontWeight: FontWeight.bold,
-              color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.more_vert_sharp),
-              style: IconButton.styleFrom(
-                foregroundColor: Colors.white,
-              )),
-        ],
-        elevation: 70,
-      ),
 
+
+
+
+/*
       body: SingleChildScrollView(
         child: Center(
           child: Wrap(
@@ -74,7 +57,7 @@ class Home extends StatelessWidget {
                 onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => Mood(),)),
                 child: Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.all(9.0),
+                    padding: const EdgeInsets.all(8.0),
         
                     child: Container(
                       alignment: Alignment.bottomCenter,
@@ -334,6 +317,7 @@ class Home extends StatelessWidget {
           ),
         ),
       )
+*/
 
     );
   }
